fd3cbbd83e53ece346e2f2cffc76cb19eea13512
saitej34 <saiteja6734.yelagandula@gmail.com>
2023-05-17 03:29:01 +0530

    yst

--- model/ResultSchema.js
+++ model/ResultSchema.js
@@ -1,29 +1,27 @@
+const mongoose = require("mongoose");
+
 const resultSchema = new mongoose.Schema({
     email:{
         type: String,
-        required: true,
-    },
-    answers: {
-        type: [String],
         required: true,
     },
     score: {
         type: Number,
         required: true,
     },
-    // date: {
-    //     type: date,
-    //     required: true,
-    // },
-    // time: {
-    //     type: String,
-    //     required: true,
-    // },
-    // question: {
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'Question',
-    //     required: true,
-
-    // },
+    date: {
+        type: Date,
+        required: true,
+    },
+    userid:{
+        type:String,
+        required:true
+    },
+    examtype:{
+        type:String,
+        required:true
+    }
     })
     const Result = mongoose.model('Result', resultSchema);
+
+module.exports = Result;
